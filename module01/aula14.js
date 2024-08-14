b37394b77f9a125abda2db468b566c93f81bfd01
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-08-13 21:50:52 -0300

    aula 13-08

--- module01/aula14.js
+++ module01/aula14.js
@@ -1,12 +1,18 @@
 //estruturas de dados
 
+ // a lista permite retirar e inserir em qualquer lugar
+
+ // fila geralmente vc insere sempre no final e retira no inicio
+
+ // a pilha insere no final e retira no final
+
 //Fila
  class Node{
     constructor(data){
         this.data = data;
-        this.next = next;
+        this.next = null;
     }
- }
+}
 
  class LinkedList{
     constructor(){
@@ -16,8 +22,8 @@
 
     add(data){
         let newNode = new Node(data);
-        if(this.head == null){
-            this.head = data;
+        if(this.head === null){
+            this.head = newNode;
         }else{
             let current = this.head;
             while(current.next !== null){
@@ -27,4 +33,85 @@
         }
         this.size++;
     }
+
+    insertAt(data, index){
+        if(index < 0 || index > this.size){
+            return console.log("index fora dos limetes!");
+        }
+
+        let newNode = new Node(data);
+        let current = this.head;
+        let previous;
+
+        if(index === 0){
+            newNode.next = this.head;
+            this.head = newNode;
+        }else{
+            for(let i = 0; i< index; i++){
+                previous = current;
+                current = current.next;
+            }
+            newNode.next = current;
+            previous.next = newNode;
+        }
+    }
+
+    removeFrom(index){
+        if(index < 0 || index > this.size){
+            return console.log("index fora dos limetes!");
+        }
+
+        let current = this.head;
+        let previous;
+
+        if(index === 0){
+            this.head = current.next;
+        }else{
+            for(let i = 0; i < index; i++){
+                previous = current;
+                current = current.next; 
+            }
+        }
+        this.size -= 1;
+        return current.data;
+    }
+
+    sizeOfList(){
+        return this.size;
+    }
+
+    isEmpty(){
+        return this.size === 0;
+    }
+    
+    indexOf(data){
+        let current = this.head;
+        let index = 0;
+
+        while(current != null){
+            if(current.data == null){
+                return index;
+            }
+            index++;
+            current = current.next
+        }
+        return -1;
+    }
+    
+    printList(){
+        let current = this.head;
+        while(current != null){
+            console.log(current.data);
+            current = current.next;
+        }
+        console.log("=========");
+    }
  }
+
+ let list = new LinkedList();
+ list.add(1);
+ list.add(true);
+ list.add("teste");
+ list.printList();
+ list.insertAt("novoElemento", 0);
+ list.printList();
