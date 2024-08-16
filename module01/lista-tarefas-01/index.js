a91d2c4052c217712d7ad45aaf893f3879f75102
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-08-16 18:56:04 -0300

    feat: finalizando os exercicios do modulo 01

--- module01/lista-tarefas-01/index.js
+++ module01/lista-tarefas-01/index.js
@@ -1,7 +1,7 @@
+const prompt = require('prompt-sync')();
+
 // 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
 // utilizando uma estrutura de controle if.
-
-// const prompt = require('prompt-sync')();
 
 // let oddOrEven = Number(prompt("insira o primeiro numero: "));
 
@@ -15,7 +15,6 @@
 //     adolescente, adulto, idoso) com base no valor fornecido, utilizando uma estrutura de
 //     controle if-else.
 
-// const prompt = require('prompt-sync')();
 // let age = Number(prompt("insira a idade a ser verificada: "));
 
 // if(age >= 18){
@@ -29,7 +28,6 @@
 // 3. Implemente um programa que recebe uma nota de 0 a 10 e classifica como
 // "Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
 
-// const prompt = require('prompt-sync')();
 // let nota = Number(prompt("insira a sua nota: "));
 
 // if(nota >= 7){
@@ -43,7 +41,6 @@
 // 4. Crie um menu interativo no console que oferece ao usuário a escolha de três opções.
 // Utilize switch-case para implementar a lógica des cada opção selecionada.
 
-// const prompt = require('prompt-sync')();
 // console.log("O que você deseja fazer?");
 // console.log("1 - Teste1");
 // console.log("2 - Teste2");
@@ -70,7 +67,6 @@
 // determina a categoria de peso (baixo peso, peso normal, sobrepeso, obesidade)
 // utilizando if-else.
 
-// const prompt = require('prompt-sync')();
 // let peso = Number(prompt("insira o seu peso: "));
 // let altura = Number(prompt("insira a sua altura: "));
 
@@ -102,7 +98,6 @@
 // Triângulo escaleno: possui todos os lados diferentes (A<>B e B <> C)
 // Triângulo eqüilátero: possui todos os lados iguais (A=B e B=C)
 
-// const prompt = require('prompt-sync')();
 // let primeiroLado = Number(prompt("insira o primeiro lado do seu triangulo: "));
 // let segundoLado = Number(prompt("insira o segundo lado do seu triangulo: "));
 // let terceiroLado = Number(prompt("insira o terceiro lado do seu triangulo: "));
@@ -123,8 +118,6 @@
 // forem compradas pelo menos doze. Escreva um algoritmo que leia o número de maçãs
 // compradas, calcule e escreva o valor total da compra.
 
-// const prompt = require('prompt-sync')();
-
 // let amountApples = Number(prompt("Quantas maçãs você deseja comprar?"));
 
 // if(amountApples < 0){
@@ -138,12 +131,14 @@
 // 8. Escreva um algoritmo para ler 2 valores (considere que não serão lidos valores iguais)
 // e escreve-los em ordem crescente.
 
-let greater = Number.MIN_VALUE;
-let lesser = Number.MAX_VALUE;
+// let numero1 = Number(prompt("Insira o primeiro valor:"));
+// let numero2 = Number(prompt("Insira o segundo valor:"));
 
-const prompt = require('prompt-sync')();
-// let primeiroValor = Number(prompt("Insira o primeiro valor:"));
-// let segundoValor = Number(prompt("Insira o segundo?"));
+// if (numero1 < numero2) {
+//     console.log("Ordem crescente: " + numero1 + ", " + numero2);
+// } else {
+//     console.log("Ordem crescente: " + numero2 + ", " + numero1);
+// }
 
 // 9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console
 // utilizando um loop for.
@@ -153,3 +148,86 @@ const prompt = require('prompt-sync')();
 // }
 
 // 10. Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+
+// let inteiro = Number(prompt("Insira um número inteiro:"));
+
+// if(Number.isInteger(inteiro)){
+//     for(let i = 1 ; i <= 10; i++){
+//         console.log(inteiro);
+//     }
+// }else{
+//     console.log("Erro, o número precisa ser inteiro");
+// }
+
+// 11. Escreva um programa que solicita ao usuário 5 números e calcula a soma total
+// utilizando um loop for.
+
+// let soma = 0;
+
+// for(let i = 1 ; i <= 5 ; i++){
+//     soma += Number(prompt("Insira um número:"));
+// }s
+
+// console.log(`A soma total é: ${soma}`);
+
+// 12. Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a
+//     10) utilizando um loop for.
+
+// let numero = Number(prompt("Insira um número:"));;
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(`${numero} x ${i} = ${numero*i}`);
+// }
+
+// 13. Fazer um algoritmo para receber números decimais até que o usuário digite 0 e fazer
+// a média aritmética desses números.
+
+// let soma = 0;
+// let contadorNumeros = 0;
+// let loopCheck = 2;
+
+// do{
+//     soma += Number(prompt("Insira um número:"));
+//     contadorNumeros++;
+
+//     loopCheck = Number(prompt("Deseja continuar? (1- sim; 2- não)"));
+// }while(loopCheck==1);
+
+// console.log(`A média aritmética dos números inseridos é: ${soma/contadorNumeros}`);
+
+// 14. Crie um programa que calcula o fatorial de um número fornecido pelo usuário
+// utilizando um loop for ou while.
+
+// let numero = Number(prompt("Qual o fatorial a ser calculado?"));
+
+// let fatorial = 1;
+
+// if(numero < 0){
+//     console.log("Erro! Não existe fatorial de numero negativo!");
+// }else if(numero == 0 || numero == 1){
+//     console.log("O fatorial de zero ou um é 1.");
+// } else {
+//     for(let i = numero ; i > 0; i--){
+//         fatorial *= i;
+//     }
+//     console.log(`O fatorial de ${numero} é ${fatorial}`);
+// }
+
+// 15. Escreva um programa que gera e imprime os primeiros 10 números da sequência de
+// Fibonacci utilizando um loop for.
+
+// let fibo1 = 1;
+// let fibo2 = 1;
+// let fiboNext;
+// let contador = 0;
+
+// console.log(`o 1° número de sequência de fibonacci é ${fibo1}`);
+// console.log(`o 2° número de sequência de fibonacci é ${fibo2}`);
+
+// while(contador != 8){
+//     let fiboNext = fibo1 + fibo2;
+//     fibo1 = fibo2;
+//     fibo2 = fiboNext;
+//     contador++;
+//     console.log(`o ${contador+2}° número de sequência de fibonacci é ${fibo2}`);
+// }
